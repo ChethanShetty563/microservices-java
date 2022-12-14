0716a4a532c483d1aa2a39d1043da6cba11424ae
ChethanShetty563 <chethan563@gmail.com>
2022-12-14 18:05:20 +0530

    Adding the code to pass the unit test

--- multiplication/src/main/java/microservices/book/multiplication/challenge/ChallengeGeneratorServiceImpl.java
+++ multiplication/src/main/java/microservices/book/multiplication/challenge/ChallengeGeneratorServiceImpl.java
@@ -1,8 +1,35 @@
 package microservices.book.multiplication.challenge;
 
+import java.util.Random;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class ChallengeGeneratorServiceImpl implements ChallengeGeneratorService {
+	
+	private final static int MINIMUM_FACTOR = 11;
+	private final static int MAXIMUM_FACTOR = 100;
+	
+	
+	private final Random random;
+	
+	public ChallengeGeneratorServiceImpl() {
+		this.random = new Random();
+	}
+	
+	protected ChallengeGeneratorServiceImpl(final Random random) {
+        this.random = random;
+    }
+	
+	private int next() {
+		return random.nextInt(MAXIMUM_FACTOR - MINIMUM_FACTOR) + MINIMUM_FACTOR;
+	}
+
+	@Override
+	public Challenge randomChallenge() {
+		// TODO Auto-generated method stub
+		return new Challenge(next(), next());
+	}
+	
 
 }
